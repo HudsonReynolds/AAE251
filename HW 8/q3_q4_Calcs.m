71637648af62bcbf0c6e64367962cd33ec1c501e
HudsonReynolds <73318449+HudsonReynolds@users.noreply.github.com>
2024-11-07 15:27:45 -0500

    calc updates

--- HW 8/q3_q4_Calcs.m
+++ HW 8/q3_q4_Calcs.m
@@ -1,33 +1,32 @@
-S = 39.4
+S = 39.4;
 
-b = 19.78
+b = 19.78;
 
-AR = b^2 / S
-
-
-powerMax = 1342.26 * 2 * 1000
+AR = b^2 / S;
 
 
-[~,~,~,rho_h] = atmosisa(2709.062) 
+powerMax = 1342.26 * 2 * 1000;
 
 
-[~,~,~,rho_0] = atmosisa(0) 
+[~,~,~,rho_h] = atmosisa(2709.062); 
 
 
-W = 10500
-
-g = 9.81
-
-e  = 0.7
-
-m = 1
-
-eta = 0.8
+[~,~,~,rho_0] = atmosisa(0);
 
 
-V = 156.464
+W = 10500;
 
-cD0 = ((rho_h/rho_0)^m * powerMax * eta - (2 * (W * g)^2) / (rho_h * S * V * pi * e * AR)) / (1/2 * rho_h * S * V^3)
+g = 9.81;
+
+e  = 0.7;
+
+m = 1;
+
+eta = 0.8;
+
+V = 156.464;
+
+cD0 = ((rho_h/rho_0)^m * powerMax * eta - (2 * (W * g)^2) / (rho_h * S * V * pi * e * AR)) / (1/2 * rho_h * S * V^3);
 
 % symbolically:
 
@@ -35,7 +34,7 @@ syms x
 
 eqn = (rho_h/rho_0)^m * powerMax * eta == 1/2 * rho_h * S * V^3 * x + 2 * (W * g)^2 / (rho_h * S * V * pi * e * AR);
 
-sol = solve(eqn, x)
+sol = solve(eqn, x);
 
 fprintf("%.6f\n", sol)
 
@@ -46,40 +45,36 @@ densityConversion = 0.062428;
 
 S = 424;
 
-b = 64.833
+b = 64.833;
 
-AR = b^2 / S
+AR = b^2 / S;
 
-powerMax = 990000 * 2
+powerMax = 990000 * 2;
 
-[~,~,~,rho_h] = atmosisa(2709.062)
+[~,~,~,rho_h] = atmosisa(2709.062);
 
 rho_h = rho_h * densityConversion;
 
-[~,~,~,rho_0] = atmosisa(0)
+[~,~,~,rho_0] = atmosisa(0);
 
 rho_0 = rho_0 * densityConversion;
 
 
-W = 23149
+W = 23149;
 
-g = 32.174
+g = 32.174;
 
-e  = 0.7
+e  = 0.7;
 
-m = 1
+m = 1;
 
-eta = 0.8
+eta = 0.8;
 
-
-V = 513.33
-
-cD0 = ((rho_h/rho_0)^m * powerMax * eta - (2 * (W)^2) / (rho_h * S * V * pi * e * AR)) / (1/2 * rho_h * S * V^3);
-
+V = 513.33;
 
 eqn = (rho_h/rho_0)^m * powerMax * eta == 1/2 * rho_h / g * S * V^3 * x + 2 * (W)^2 / (rho_h /g * S * V * pi * e * AR);
 
-sol = solve(eqn, x)
+sol = solve(eqn, x);
 
 fprintf("%.6f\n", sol)
 
