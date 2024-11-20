72a77437923b7c1889932805d6e1543e08a3d544
HudsonReynolds <73318449+HudsonReynolds@users.noreply.github.com>
2024-11-19 19:40:40 -0500

    graph

--- AAE251_HW9_Q1cd.m
+++ AAE251_HW9_Q1cd.m
@@ -34,13 +34,37 @@ end
 %% Graphing
 
 % Output the required and available thrust with respect to altitude
-figure(1)
-plot(alt,thrustRequired)
+
+hfig = figure;  % save the figure handle in a variable
+
+
+fname = "Available and Required Thrust Vs Altitude";
+
+picturewidth = 20; % set this parameter and keep it forever
+hw_ratio = .7; % feel free to play with this ratio
+set(findall(hfig,'-property','FontSize'),'FontSize',16) % adjust fontsize to your document
+
 hold on
-plot(alt,thrustAvailable)
+
+plot(alt,thrustRequired, 'linewidth', 1)
+hold on
+plot(alt,thrustAvailable, 'LineWidth',1)
 grid minor
 title("Available and Required Thrust Vs. Altitude")
 xlabel("Altitude [km]")
 ylabel("Thrust [N]")
 legend("Required Thrust", "Available Thrust", location="northeast")
+
+
+set(findall(hfig,'-property','Box'),'Box','off') % optional
+set(findall(hfig,'-property','Interpreter'),'Interpreter','latex') 
+set(findall(hfig,'-property','TickLabelInterpreter'),'TickLabelInterpreter','latex')
+set(hfig,'Units','centimeters','Position',[3 3 picturewidth hw_ratio*picturewidth])
+pos = get(hfig,'Position');
+set(hfig,'PaperPositionMode','Auto','PaperUnits','centimeters','PaperSize',[pos(3), pos(4)])
+%print(hfig,fname,'-dpdf','-painters','-fillpage')
+print(hfig,fname,'-dpng','-r300')
+
+
+
 
